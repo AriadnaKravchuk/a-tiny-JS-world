30f1f609edcbf145e936ce906a49cf10046ca593
AriadnaKravchuk <asdfgh.qa198@gmail.com>
2022-09-15 02:32:20 +0300

    Fix bug when add friends or no add

--- index.js
+++ index.js
@@ -28,15 +28,22 @@ class Inhabitant {
       this.attributes = [];
    }
 
-   addFriends(...friend) {
-      if (!this.friends.includes(friend)) this.friends.push(friend);
+   addFriends(...friends) {
+      for (let i = 0; i < friends.length; i++) {
+         if (!this.friends.includes(friends[i])) {
+            this.friends.push(friends[i]);
+         }
+      }
    }
 
-   printAttributes() {
-      this.friends.forEach((objFriends) => {
-         print(this.attributes.join("; ") + "; " + 
-            objFriends.map((friend) => friend.name))
-      });
+  printAttributes() {
+      let friendsNames = [];
+
+      for (let i = 0; i < this.friends.length; i++) {
+         friendsNames.push(this.friends[i]["name"]);
+      }
+
+      print(this.attributes.join("; ") + "; " + friendsNames.join(", "));
    }
 }
 
